9c2bf7ace6509e3bba3867c0c5b69a21886efb8a
UpperCod <uppercod.git@gmail.com>
2022-09-22 12:02:49 -0300

    fix DOM recycling

--- packages/storybook/src/decorator.ts
+++ packages/storybook/src/decorator.ts
@@ -5,11 +5,20 @@ const cache: {
     [id: string]: Element;
 } = {};
 
+class Wrapper extends HTMLElement {
+    disconnectedCallback() {
+        delete cache[this.dataset.id];
+    }
+}
+
+customElements.define("atomico-decorator-wrapper", Wrapper);
+
 export const decorator: DecoratorFunction = (Story, context) => {
     if (!cache[context.id]) {
-        cache[context.id] = document.createElement("div");
-        cache[context.id].className = "atomico-decorator-wrapper";
+        cache[context.id] = document.createElement("atomico-decorator-wrapper");
+        cache[context.id].setAttribute("data-id", context.id);
     }
+
     let result = Story() as
         | any[]
         | { render?: (prev: Element) => Element; $$: symbol }
@@ -23,7 +32,8 @@ export const decorator: DecoratorFunction = (Story, context) => {
     } else if (result instanceof Node) {
         return result;
     } else if (Array.isArray(result) || result.$$) {
-        return render(h("host", null, result), cache[context.id]);
+        render(h("host", null, result), cache[context.id]);
+        return cache[context.id];
     } else if (result.render) {
         result.render(cache[context.id]);
         return cache[context.id];
